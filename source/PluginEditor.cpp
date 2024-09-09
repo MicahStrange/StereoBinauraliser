ea045ef2e681805c28d22386f81356b6a110d7b7
Micah Strange <micahstrange16@gmail.com>
2024-09-09 18:15:26 +0100

    with hot reloading

--- source/PluginEditor.cpp
+++ source/PluginEditor.cpp
@@ -1,48 +1,125 @@
 #include "PluginEditor.h"
 
-PluginEditor::PluginEditor (PluginProcessor& p)
-    : AudioProcessorEditor (&p), processorRef (p)
+const juce::String PluginEditor::kLocalDevServerAddress = "http://localhost:5173/";
+
+bool PluginEditor::SinglePageBrowser::pageAboutToLoad (const juce::String & newURL)
+{
+    return newURL == PluginEditor::kLocalDevServerAddress || newURL == getResourceProviderRoot ();
+}
+
+std::vector<std::byte> StreamToVector (juce::InputStream & stream)
+{
+    // Workaround to make ssize_t work cross-platform.
+    using namespace juce;
+    const auto sizeInBytes = static_cast<size_t> (stream.getTotalLength ());
+    std::vector<std::byte> result (sizeInBytes);
+    stream.setPosition (0);
+    [[maybe_unused]] const auto bytesRead = stream.read (result.data (), result.size ());
+    jassert (bytesRead == static_cast<ssize_t> (sizeInBytes));
+    return result;
+}
+
+static const char * GetMimeForExtension (const juce::String & extension)
+{
+    static const std::unordered_map<juce::String, const char *> mimeMap = {
+        {{"htm"}, "text/html"},
+        {{"html"}, "text/html"},
+        {{"txt"}, "text/plain"},
+        {{"jpg"}, "image/jpeg"},
+        {{"jpeg"}, "image/jpeg"},
+        {{"svg"}, "image/svg+xml"},
+        {{"ico"}, "image/vnd.microsoft.icon"},
+        {{"json"}, "application/json"},
+        {{"png"}, "image/png"},
+        {{"css"}, "text/css"},
+        {{"map"}, "application/json"},
+        {{"js"}, "text/javascript"},
+        {{"woff2"}, "font/woff2"}};
+
+    if (const auto it = mimeMap.find (extension.toLowerCase ()); it != mimeMap.end ())
+        return it->second;
+
+    jassertfalse;
+    return "";
+}
+
+static juce::File GetAssetsDirectory ()
+{
+#if JUCE_MAC
+    auto assets_dir =
+        juce::File::getSpecialLocation (juce::File::SpecialLocationType::currentApplicationFile)
+            .getChildFile ("Contents/Resources/dist");
+#elif JUCE_WINDOWS
+    auto assets_dir = juce::File::getSpecialLocation (
+                          juce::File::SpecialLocationType::
+                              currentExecutableFile) // Plugin.vst3/Contents/<arch>/Plugin.vst3
+                          .getParentDirectory ()     // Plugin.vst3/Contents/<arch>/
+                          .getParentDirectory ()     // Plugin.vst3/Contents/
+                          .getChildFile ("Resources/dist");
+#else
+    #error "We only support Mac and Windows here yet."
+#endif
+
+    return assets_dir;
+}
+
+std::optional<juce::WebBrowserComponent::Resource>
+PluginEditor::GetResource (const juce::String & url)
+{
+    std::cout << "ResourceProvider called with " << url << std::endl;
+
+    static const auto resourceFilesRoot =
+        juce::File::getSpecialLocation (juce::File::SpecialLocationType::currentApplicationFile)
+            .getParentDirectory ()
+            .getParentDirectory ()
+            .getChildFile ("public");
+
+    [[maybe_unused]] static auto printRootOnce = []
+    {
+        std::cout << "Resource files root is " << resourceFilesRoot.getFullPathName () << std::endl;
+        return true;
+    }();
+
+    const auto resourceToRetrieve =
+        url == "/" ? "index.html" : url.fromFirstOccurrenceOf ("/", false, false);
+
+    const auto resource = resourceFilesRoot.getChildFile (resourceToRetrieve).createInputStream ();
+    if (resource)
+    {
+        const auto extension = resourceToRetrieve.fromLastOccurrenceOf (".", false, false);
+        return juce::WebBrowserComponent::Resource {StreamToVector (*resource),
+                                                    GetMimeForExtension (extension)};
+    }
+
+    return std::nullopt;
+}
+
+PluginEditor::PluginEditor (PluginProcessor & p)
+    : AudioProcessorEditor (&p)
+    , processorRef (p)
+    , web_browser_component_ (kBaseWebOptions)
 {
     juce::ignoreUnused (processorRef);
+    asset_directory_ = GetAssetsDirectory ();
 
-    addAndMakeVisible (inspectButton);
+    setSize (400, 300);
 
-    // this chunk of code instantiates and opens the melatonin inspector
-    inspectButton.onClick = [&] {
-        if (!inspector)
-        {
-            inspector = std::make_unique<melatonin::Inspector> (*this);
-            inspector->onClose = [this]() { inspector.reset(); };
-        }
-
-        inspector->setVisible (true);
-    };
-
+    // #if DEV_LOCALHOST
+    //     web_browser_component_.goToURL (kLocalDevServerAddress);
+    // #else
+    //     web_browser_component_.goToURL (juce::WebBrowserComponent::getResourceProviderRoot ());
+    // #endif
+    web_browser_component_.goToURL (kLocalDevServerAddress);
+    addAndMakeVisible (web_browser_component_);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
 }
 
-PluginEditor::~PluginEditor()
+PluginEditor::~PluginEditor ()
 {
 }
 
-void PluginEditor::paint (juce::Graphics& g)
+void PluginEditor::resized ()
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    auto area = getLocalBounds();
-    g.setColour (juce::Colours::white);
-    g.setFont (16.0f);
-    auto helloWorld = juce::String ("Hello from ") + PRODUCT_NAME_WITHOUT_VERSION + " v" VERSION + " running in " + CMAKE_BUILD_TYPE;
-    g.drawText (helloWorld, area.removeFromTop (150), juce::Justification::centred, false);
-}
-
-void PluginEditor::resized()
-{
-    // layout the positions of your child components here
-    auto area = getLocalBounds();
-    area.removeFromBottom(50);
-    inspectButton.setBounds (getLocalBounds().withSizeKeepingCentre(100, 50));
+    web_browser_component_.setBounds (getLocalBounds ());
 }
