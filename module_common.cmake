a0e5fa336da441457ac8a16a086cb53380a9662c
Micah Strange <micahstrange16@gmail.com>
2024-10-05 13:49:06 +0100

    cmake

---
+++ module_common.cmake
@@ -0,0 +1,29 @@
+set(MODULE_LINK_CONF
+        juce::juce_recommended_config_flags juce::juce_recommended_lto_flags
+        juce::juce_recommended_warning_flags)
+
+function(add_module)
+    cmake_parse_arguments(PARSED_ARGS "" "MODULE" "SOURCES;DEPS" ${ARGN})
+
+    if (NOT PARSED_ARGS_MODULE)
+        message(FATAL_ERROR "You must provide a module name")
+    endif (NOT PARSED_ARGS_MODULE)
+
+    add_library(${PARSED_ARGS_MODULE} STATIC ${PARSED_ARGS_SOURCES})
+    target_include_directories(${PARSED_ARGS_MODULE}
+            PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/src)
+    target_link_libraries(${PARSED_ARGS_MODULE} PUBLIC ${MODULE_LINK_CONF}
+            ${PARSED_ARGS_DEPS})
+endfunction(add_module)
+
+function(add_test)
+    cmake_parse_arguments(PARSED_ARGS "" "MODULE" "SOURCES;DEPS" ${ARGN})
+
+    set(BINARY_TST ${PARSED_ARGS_MODULE}_test)
+    add_executable(${BINARY_TST})
+    target_sources(${BINARY_TST} PRIVATE ${PARSED_ARGS_SOURCES})
+    target_link_libraries(
+            ${BINARY_TST}
+            PRIVATE ${PARSED_ARGS_DEPS} Catch2::Catch2WithMain
+            PUBLIC ${MODULE_LINK_CONF})
+endfunction(add_test)
